0c6bfe17a73c85b1fb2d17ad5cf4c6ebf4178d93
Quan Yang <QuanYang@live.com>
2014-05-14 22:45:36 +0800

    minor tweaks to line drawing and new moduleBlock

--- Prototype/js/script.js
+++ Prototype/js/script.js
@@ -18,10 +18,10 @@ function redrawLine(e){
                 var off2 = getOffset(document.getElementById(dblock));
                 // bottom right
                 var x1 = off1.left + off1.width/2;
-                var y1 = off1.top + off1.height + $("body").scrollTop()-1;
+                var y1 = off1.top + off1.height + $("body").scrollTop();
                 // top right
                 var x2 = off2.left + off2.width/2;
-                var y2 = off2.top  + $("body").scrollTop();
+                var y2 = off2.top  + $("body").scrollTop()+1;
                 // distance
                 var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
                 // center
@@ -52,10 +52,10 @@ function connect(div1, div2, color, thickness) { // draw a line connecting eleme
     var off2 = getOffset(document.getElementById(div2));
     // bottom right
     var x1 = off1.left + off1.width/2;
-    var y1 = off1.top + off1.height + $("body").scrollTop()-1;
+    var y1 = off1.top + off1.height + $("body").scrollTop();
     // top right
     var x2 = off2.left + off2.width/2;
-    var y2 = off2.top  + $("body").scrollTop();
+    var y2 = off2.top  + $("body").scrollTop()+1;
     // distance
     var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
     // center
@@ -64,7 +64,7 @@ function connect(div1, div2, color, thickness) { // draw a line connecting eleme
     // angle
     var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
     // make hr
-    var htmlLine = "<div class='connect "+div1+" "+div2+"' style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
+    var htmlLine = "<div class='connect "+div1+" "+div2+"' style='z-index:-1;padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
     //
     // alert(htmlLine);
     $("body").append(htmlLine);
